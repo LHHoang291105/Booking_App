42af564a60d1aaff4f841315480ee8c776cfee2d
Hoang Le <lhoangimt@gmail.com>
2025-08-08 23:04:58 +0700

    booking-app-byHoangBe

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import "./home.css" 
+import { Navbar } from "../../components/navbars/Navbar";
+import Header from "../../components/header/Header";
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Header/>
+    </div>
+  )
+}
+export default Home;
