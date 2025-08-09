208c27a239dd030f18e58c55b37a95a58e106931
Hoang Le <lhoangimt@gmail.com>
2025-08-09 23:22:27 +0700

    Cap nhat lan 2

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,130 @@
-import "./header.css"
+import "./header.css";
+import { Children, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBed,
+  faCalendarDays,
+  faCar,
+  faPerson,
+  faPlane,
+  faTaxi,
+} from "@fortawesome/free-solid-svg-icons";
+import { DateRange } from "react-date-range";
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css";
+import { enUS } from "date-fns/locale";
+import { format } from "date-fns";
 
 const Header = () => {
+  const [openDate, setOpenDate] = useState(false);
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
+  const [openOptions, setOpenOptions] = useState(false);
+  const [options, setOptions] = useState({
+    adult: 1,
+    children: 0,
+    room: 1,
+  });
   return (
-    <div className="header">Header</div>
-  )
-}
+    <div className="header">
+      <div className="headerContainer">
+        <div className="headerList">
+          <div className="headerListItem active">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car rentals</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Atractions</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faTaxi} />{" "}
+          </div>
+        </div>
+        <h1 className="headerTitle">A lifetime of discounts? It's Genius.</h1>
+        <p className="headerDesc">
+          Get rewarded for your travels – unlock instant savings of 10% or more
+          with a free Lamabooking account
+        </p>
+        <button className="headerBtn">Sign in / Register</button>
+        <div className="headerSearch">
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faBed} className="headerIcon" />
+            <input
+              type="text"
+              placeholder="Where are you going?"
+              className="headerSearchInput"
+            />
+          </div>
 
-export default Header
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+            <span
+              onClick={() => setOpenDate(!openDate)}
+              className="headerSearchText"
+            >{`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(
+              date[0].endDate,
+              "MM/dd/yyyy"
+            )}`}</span>
+            {openDate && (
+              <DateRange
+                locale={enUS}
+                editableDateInputs={true}
+                onChange={(item) => setDate([item.selection])}
+                moveRangeOnFirstSelection={false}
+                ranges={date}
+                className="date"
+              />
+            )}
+          </div>
+
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+            <span className="headerSearchText">{`${options.adult} adult . ${options.children} children . ${options.room} room `}</span>
+            <div className="options">
+              <div className="optionsItem">
+                <span className="optionText">Adult</span>
+                <button className="optionCounter">-</button>
+                <span className="optionCounterNumber">1</span>
+                <button className="optionCounter">+</button>
+              </div>
+
+              <div className="optionsItem">
+                <span className="optionText">Children</span>
+                <button className="optionCounter">-</button>
+                <span className="optionCounterNumber">1</span>
+                <button className="optionCounter">+</button>
+              </div>
+
+              <div className="optionsItem">
+                <span className="optionText">Room</span>
+                <button className="optionCounter">-</button>
+                <span className="optionCounterNumber">0</span>
+                <button className="optionCounter">+</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="headerSearchItem">
+            <button className="headerBtn">Search</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,10 +2,18 @@ import "./home.css"
 import { Navbar } from "../../components/navbars/Navbar";
 import Header from "../../components/header/Header";
 const Home = () => {
+/**
+ * Home component serves as the main page
+ * that renders the Navbar and Header components.
+ * 
+ * @returns {JSX.Element} The rendered component
+ */
   return (
     <div>
       <Navbar/>
+      {/* Render the Navbar component */}
       <Header/>
+      {/* Render the Header component */}
     </div>
   )
 }
